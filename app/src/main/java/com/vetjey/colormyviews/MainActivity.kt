4c00639295db5a9c861e0bc0cb14629c353e5908
Vetjey <vetjeyapp@gmail.com>
2019-03-03 10:25:54 +0300

    First commit

---
+++ app/src/main/java/com/vetjey/colormyviews/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.vetjey.colormyviews
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListener()
+    }
+
+    private fun setListener(){
+        val clickableViews:List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text,
+            box_five_text, constraint_layout, red_button, yellow_button, green_button)
+
+        for (item in clickableViews){
+            item.setOnClickListener { makeColored(it)}
+        }
+    }
+
+    private fun makeColored(view: View){
+        when (view.id){
+
+            //Use color class
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            //Use Android color resources
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            //Use custom colors
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
+
